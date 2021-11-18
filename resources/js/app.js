4fdc62ee7f51b9919097c3e3ef1b60d3dae5f66c
wiki chua <wikichua@gmail.com>
2021-11-18 16:54:00 +0800

    latest inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,33 +2,30 @@ require('./bootstrap');
 
 // Import modules...
 import { createApp, h } from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Head, Link} from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress';
 import { createPopper } from "@popperjs/core";
 import '@fortawesome/fontawesome-free/js/all.min';
 import mitt from 'mitt';
 
-const el = document.getElementById('app');
+createInertiaApp({
+  resolve: name => require(`../vues/Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+        const app = createApp({ render: () => h(App, props) })
+            .mixin({ methods: { route } })
+            .component('InertiaHead', Head)
+            .component('InertiaLink', Link)
+            .use(plugin);
+        let req = require.context('../vues/Components/', true, /\.(js|vue)$/i);
+        req.keys().map(key => {
+            let name = key.match(/\w+/)[0];
+            app.component('instant' + name, req(key).default);
+        });
 
-const app = createApp({
-    render: () =>
-    h(InertiaApp, {
-        initialPage: JSON.parse(el.dataset.page),
-        resolveComponent: (name) => require(`../vues/Pages/${name}`).default,
-    }),
-})
-.mixin({ methods: { route } })
-.use(InertiaPlugin);
-
-let req = require.context('../vues/Components/', true, /\.(js|vue)$/i);
-req.keys().map(key => {
-    let name = key.match(/\w+/)[0];
-    return app.component('instant' + name, req(key).default);
+        const emitter = mitt();
+        app.config.globalProperties.emitter = emitter;
+        return app.mount(el);
+    },
 });
-
-const emitter = mitt();
-app.config.globalProperties.emitter = emitter;
-
-app.mount(el);
 
 InertiaProgress.init({ color: '#4B5563' });
